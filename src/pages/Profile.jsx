58d55a4f490178a919482e4b47c9818af6e265f7
Ashik k <ashikash796@gmail.com>
2024-03-27 15:42:06 +0530

    feat(E-Commerce):Designed Orders Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,7 +8,7 @@ function Profile() {
   };
 
   return (
-    <div className="mx-auto mt-8">
+    <div className="mx-auto mt-8 ">
       <div className="mb-4">
         <h2 className="text-lg font-semibold">Name: {user.name}</h2>
         <p className="text-gray-600">Email: {user.email}</p>
